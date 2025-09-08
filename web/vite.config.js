3d863337b5e369bf60abd89c7b41b2663c90f23b
Rokas Čiuplinskas <rokas.roktantu@gmail.com>
2025-09-08 20:41:12 +0300

    fix host

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   server: {
-    allowedHosts: ("karkasai.ciuplinskas.lt"),
+    allowedHosts: ['karkasai.ciuplinskas.lt'],
     proxy: { '/api': 'http://app:8080' }
   }
 })
