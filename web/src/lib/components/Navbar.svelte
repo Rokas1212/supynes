436692e7d86f7c4e74148791759d1e8196467c4b
Rokas Čiuplinskas <rokas.roktantu@gmail.com>
2025-09-11 16:25:12 +0300

    style

--- web/src/lib/components/Navbar.svelte
+++ web/src/lib/components/Navbar.svelte
@@ -60,7 +60,7 @@
 				<div class="ml-4 sm:hidden">
 					<button
 						type="button"
-						class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+						class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
 						aria-expanded={isOpen}
 						on:click={() => (isOpen = !isOpen)}
 					>
